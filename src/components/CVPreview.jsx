0ba897ca13ecc07293685bca20a8891c59782c1e
PandesalPanpan <petermarticio@gmail.com>
2025-10-15 12:15:33 +0800

    Feat: Initial structure

---
+++ src/components/CVPreview.jsx
@@ -0,0 +1,9 @@
+export default function CVPreview() {
+
+    return (
+        <div className="cv-preview">
+            <h1>Peter Marticio</h1>
+            <p>Email</p>
+        </div>
+    )
+}
---
+++ src/components/GeneralInformation.jsx
@@ -0,0 +1,10 @@
+export default function GeneralInformation() {
+
+    return (
+        <section className="general-information">
+            <h1>Name</h1>
+            <p>Email</p>
+            <p>Phone Number</p>
+        </section>
+    )
+}
