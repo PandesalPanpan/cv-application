e4656bed654da3bd3896983d0b2a29fcc6528cca
PandesalPanpan <petermarticio@gmail.com>
2025-10-15 13:48:23 +0800

    Feat: Add Educational props and temporary structure in Practical Experience

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -8,17 +8,25 @@ export default function CVPreview() {
     return (
         <div className="cv-preview">
             <GeneralInformationPreview
-            name={"Alison Burgers"}
-            github={"github.com/PandesalPanpan"}
-            gmail={"alisonburger@gmail.com"}
-            number={"(63+) 912 345 6789"}
+                name={"Alison Burgers"}
+                github={"github.com/PandesalPanpan"}
+                gmail={"alisonburger@gmail.com"}
+                number={"(63+) 912 345 6789"}
             />
             <hr className="section-separator"/>
-            <EducationalExperiencePreview>
-
-            </EducationalExperiencePreview>
+            <EducationalExperiencePreview
+                school={"Escuela de Sophia"}
+                study={"Bachelors of Computer Engineering"}
+                date={"2023-2025"}
+            />
+            <hr className="section-separator"/>            
             <PracticalExperiencePreview>
-
+                <h3>Company Name</h3>
+                <h4>Position Title</h4>
+                <ul>
+                    <li>Responbility #1</li>
+                </ul>
+                <p>Start-End Date</p>
             </PracticalExperiencePreview>
         </div>
     )
