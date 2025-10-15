8f66f887cd4e0ccfce628b3549b5adfb134567af
PandesalPanpan <petermarticio@gmail.com>
2025-10-16 07:58:29 +0800

    Feat: Temporary objects to pass into the components

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -1,32 +1,59 @@
 import GeneralInformationPreview from "./previews/GeneralInformationPreview"
 import EducationalExperiencePreview from './previews/EducationalExperiencePreview';
-import PracticalExperiencePreview from './previews/EducationalExperiencePreview';
+import PracticalExperiencePreview from './previews/PracticalExperiencePreview';
 import '../styles/CVPreview.css';
+
+const person = {
+    name: "Alison Burgers",
+    github: "github.com/PandesalPanpan",
+    gmail: "alisonburger@gmail.com",
+    number: "(63+) 912 345 6789"
+}
+
+const companies = [
+    {
+        company: "Ntek Systems Inc",
+        position: "PHP Developer",
+        responsibilities: [
+            "Create a reporting system",
+            "API Integration"
+        ]
+    },
+    {
+        company: "Mash Garage",
+        position: "Software Developer",
+        responsibilities: [
+            "Inventory Management System"
+        ]
+    }
+]
+
+const educations = [
+    {
+        school: "Polytechnic University of the Philippines",
+        date: "2021-2027",
+        course: "Bachelors of Computer Engineering"
+    }
+]
 
 export default function CVPreview() {
 
     return (
         <div className="cv-preview">
             <GeneralInformationPreview
-                name={"Alison Burgers"}
-                github={"github.com/PandesalPanpan"}
-                gmail={"alisonburger@gmail.com"}
-                number={"(63+) 912 345 6789"}
+                name={person.name}
+                github={person.github}
+                gmail={person.gmail}
+                number={person.number}
             />
-            <hr className="section-separator"/>
+            <hr className="section-separator" />
             <EducationalExperiencePreview
-                school={"Escuela de Sophia"}
-                study={"Bachelors of Computer Engineering"}
-                date={"2023-2025"}
+                studies={educations}
             />
-            <hr className="section-separator"/>            
-            <PracticalExperiencePreview>
-                <h3>Company Name</h3>
-                <h4>Position Title</h4>
-                <ul>
-                    <li>Responbility #1</li>
-                </ul>
-                <p>Start-End Date</p>
+            <hr className="section-separator" />
+            <PracticalExperiencePreview
+                jobs={companies}
+            >
             </PracticalExperiencePreview>
         </div>
     )
