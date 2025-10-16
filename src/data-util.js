6b99a04824feaa5b4438e7757073fd529a27b82c
PandesalPanpan <petermarticio@gmail.com>
2025-10-16 11:30:47 +0800

    Feat: Add initial state data

--- src/data-util.js
+++ src/data-util.js
@@ -2,5 +2,39 @@ export const sampleGeneralInformation = {
     name: "Alison Burger",
     email: "alisonburger@gmail.com",
     phone: "(+63) 912 345 6789",
-    github: "github.com/PandesalPanpan"
+    github: "PandesalPanpan"
 }
+
+export const sampleEducations = [
+    {
+        school: "Polytechnic University of the Philippines",
+        date: "2024-2027",
+        course: "Bachelor's in Computer Engineering" 
+    },
+    {
+        school: "Insitute of Technology",
+        date: "2019-2021",
+        course: "Diploma in Computer Engineering Technology"
+    }
+]
+
+
+export const sampleCompanies = [
+    {
+        company: "Ntek Systems Inc",
+        position: "PHP Developer",
+        date: "Sept 2023 - Oct 2023",
+        responsibilities: [
+            "Create a reporting system",
+            "API Integration"
+        ]
+    },
+    {
+        company: "Mash Garage",
+        position: "Software Developer",
+        date: "2025 - Present",
+        responsibilities: [
+            "Inventory Management System"
+        ]
+    }
+]
